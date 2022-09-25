2772dd0b7a7b09ba82187724bd5127bf01c9f889
PixelMutation <charlie.e.spencer@gmail.com>
2022-09-25 19:54:54 +0100

    change configs

--- src/configuration.h
+++ src/configuration.h
@@ -13,15 +13,16 @@
 #define MIN_SPOT CYAN
 #define CENTRE_SPOT GREEN
 
-#define FILTERING 40
+#define FILTERING 100 // 0-100. lower increases filtering which reduces noise but adds latency
+#define RESOLUTION 1 // 1 is 0.01C, 100 is 1C
 
-#define TEMP_RANGE 340
-#define TEMP_MULTIPLIER 50 // gives 0.01C resolution (depending on palette type)
-#define TEMP_OFFSET 40 // we store temperatures as unsigned integers so add 40
+#define TEMP_RANGE 340 // total range of the sensor (-40 to 300C)
+#define TEMP_MULTIPLIER 100 // Temperatures are stored as unsigned integers to reduce float processing. A value of 100 means temperature is stored to the nearest 0.01C
+#define TEMP_OFFSET 40 // we store temperatures as unsigned integers, since the minimum is -40 we must add 40
 #define TEMP_OFFSET_MULTIPLIED (TEMP_OFFSET * TEMP_MULTIPLIER)
 
 #define PALETTE_SIZE (TEMP_RANGE * TEMP_MULTIPLIER)
 
-#define REFRESH MLX90640_32_HZ
+#define REFRESH MLX90640_64_HZ // 32Hz is stable, 64Hz not yet. Lower values reduce noise but obviously add lag.
 
 #endif
